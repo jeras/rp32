3d088798dbe690bc1fe70d10fb83316c9e58f6d4
Iztok Jeras <iztok.jeras@gmail.com>
2021-04-23 22:23:36 +0200

    SRC: added load/store instructions to test SW

--- src/main.s
+++ src/main.s
@@ -28,5 +28,26 @@ _start:
     addi t1, t1, 0x0a  # \n
     sw t1, 0(t0)
 
+    li t0, 0x00000000  # pointer
+    li t1, 0x00000000  # value
+    li t2, 0x00000003  # counter
+
+write:
+    sw t1, 0(t0)
+    addi t0, t0, 4
+    addi t1, t1, 1
+    addi t2, t2, -1
+    bnez t2, write   # branch non equal zero
+
+    li t0, 0x00000000  # pointer
+    li t1, 0x00000000  # value
+    li t2, 0x00000003  # counter
+
+read:
+    lw t1, 0(t0)
+    addi t0, t0, 4
+    addi t2, t2, -1
+    bnez t2, read   # branch non equal zero
+
 finish:
     beq t1, t1, finish
