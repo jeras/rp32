56839e3c199c8fe9f85dfd5c741a36312fdc8bdb
Iztok Jeras <iztok.jeras@gmail.com>
2021-04-23 21:02:34 +0200

    SETUP: added system variables for SystemC

--- settings.sh
+++ settings.sh
@@ -1,8 +1,16 @@
+# verilator path
+export VERILATOR_ROOT=$HOME/VLSI/verilator
+
+# SystemC path
+export SYSTEMC_INCLUDE=/opt/systemc-2.3.2/include/
+export SYSTEMC_LIBDIR=/opt/systemc-2.3.2/lib-linux64/
+export LD_LIBRARY_PATH=/opt/systemc-2.3.2/lib-linux64/
+
 # default OVP location
 export OVP_HOME=$HOME/Workplace/Imperas.20180716
 
-# OVP environment setup
-. $OVP_HOME/bin/setup.sh
-setupImperas -m32 $OVP_HOME
-. $OVP_HOME/bin/switchRuntime.sh
-switchRuntimeImperas
+## OVP environment setup
+#. $OVP_HOME/bin/setup.sh
+#setupImperas -m32 $OVP_HOME
+#. $OVP_HOME/bin/switchRuntime.sh
+#switchRuntimeImperas
