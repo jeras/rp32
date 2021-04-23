58eb29c88166301ad42e7a6503923abc1ed40809
Iztok Jeras <iztok.jeras@gmail.com>
2021-04-23 21:03:45 +0200

    HDL: memory model now accepts binary files

--- tbn/mem.sv
+++ tbn/mem.sv
@@ -3,7 +3,7 @@
 
 module mem #(
   // 1kB by default
-  string       FN = "",         // initialization file name
+  string       FN = "",         // binary initialization file name
   int unsigned DW = 32,         // data    width
   int unsigned SW = DW/8,       // select  width
   int unsigned SZ = 2**12,      // memory size in bytes
@@ -22,7 +22,7 @@ module mem #(
 // word address width
 localparam int unsigned WW = $clog2(SW);
 
-logic [SW-1:0][8-1:0] mem [0:SZ/SW-1];
+logic [8-1:0] mem [0:SZ-1];
 
 // write and read access
 always @(posedge clk)
@@ -30,11 +30,14 @@ if (req) begin
   if (wen) begin
     // write access
     for (int unsigned i=0; i<SW; i++) begin
-      mem[adr[AW-1:WW]][sel[i]] <= wdt[i];
+      if (sel[i])  mem[int'(adr)+i] <= wdt[i];
     end
   end else begin
     // read access
-    rdt <= mem[adr[AW-1:WW]];
+    for (int unsigned i=0; i<SW; i++) begin
+      if (sel[i])  rdt[i] <= mem[int'(adr)+i];
+      else         rdt[i] <= 'x;
+    end
   end
 end
 
@@ -45,6 +48,12 @@ assign ack = 1'b1;
 
 // initialization
 initial
-if (FN!="") $readmemh(FN, mem);
+if (FN!="") begin
+  int code;  // status code
+  int fd;    // file descriptor
+  fd = $fopen(FN, "rb");
+  code = $fread(mem, fd);
+  $fclose(fd);
+end
 
 endmodule: mem
