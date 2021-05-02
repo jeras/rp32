d5f02349bc935e193fbf9680cd0f17c67459ebb2
Iztok Jeras <iztok.jeras@gmail.com>
2021-05-02 21:07:09 +0200

    TEST: added more macros, not sure how usefull this change is

--- test/r5p/model_test.h
+++ test/r5p/model_test.h
@@ -1,20 +1,36 @@
 #ifndef _COMPLIANCE_MODEL_H
 #define _COMPLIANCE_MODEL_H
 
-// The .align 4 ensures that the signature begins at a 16-byte boundary
-#define RVMODEL_DATA_BEGIN                                              \
-  .align 4; .global begin_signature; begin_signature:
+#include "riscv_test.h"
 
-// The .align 4 ensures that the signature ends at a 16-byte boundary
-#define RVMODEL_DATA_END                                                      \
-  .align 4; .global end_signature; end_signature:
-
-//RVMODEL_BOOT
 //-----------------------------------------------------------------------
 // RV Compliance Macros
 //-----------------------------------------------------------------------
 
-#define TESTUTIL_BASE 0x10000
+#define RVMODEL_DATA_SECTION                                                  \
+        .pushsection .tohost,"aw",@progbits;                                  \
+        .align 8; .global tohost; tohost: .dword 0;                           \
+        .align 8; .global fromhost; fromhost: .dword 0;                       \
+        .popsection;                                                          \
+        .align 8; .global begin_regstate; begin_regstate:                     \
+        .word 128;                                                            \
+        .align 8; .global end_regstate; end_regstate:                         \
+        .word 4;
+
+// The .align 4 ensures that the signature begins at a 16-byte boundary
+#define RVMODEL_DATA_BEGIN                                                    \
+        .align 4; .global begin_signature; begin_signature:
+
+// The .align 4 ensures that the signature ends at a 16-byte boundary
+#define RVMODEL_DATA_END                                                      \
+        .align 4; .global end_signature; end_signature:                       \
+        RVMODEL_DATA_SECTION
+
+//-----------------------------------------------------------------------
+// RV Compliance Macros
+//-----------------------------------------------------------------------
+
+#define TESTUTIL_BASE                  0x10000
 #define TESTUTIL_ADDR_HALT             0x0
 #define TESTUTIL_ADDR_BEGIN_SIGNATURE  0x4
 #define TESTUTIL_ADDR_END_SIGNATURE    0x8
@@ -31,7 +47,10 @@
         li t0, 1;                                                             \
         sw t0, TESTUTIL_ADDR_HALT(t1);                                        \
 
-#define RVMODEL_BOOT
+
+#define RVMODEL_BOOT                                                          \
+.section .text.init;                                                          \
+        RVMODEL_IO_INIT
 
 #endif // _COMPLIANCE_MODEL_H
 
