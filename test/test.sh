4b5c3e03fbafe5643e5e8df6d37e75c28328d734
Iztok Jeras <iztok.jeras@gmail.com>
2021-05-02 21:07:41 +0200

    TEST: added shell script for running the tests

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,6 @@
-TARGETDIR=`pwd` RISCV_TARGET=r5p make -C ../submodules/riscv-arch-test
+#!/bin/sh
+
+export PATH=/home/ijeras/VLSI/lowrisc-toolchain-gcc-rv32imc-20210412-1/bin/:$PATH
+
+TARGETDIR=`pwd` RISCV_TARGET=r5p make -C ../submodules/riscv-arch-test clean
+TARGETDIR=`pwd` RISCV_TARGET=r5p make -C ../submodules/riscv-arch-test compile
