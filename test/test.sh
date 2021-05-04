74c0e9af88ff894483d3adc615ac2810b9dcedc6
Iztok Jeras <iztok.jeras@gmail.com>
2021-05-04 14:36:04 +0200

    specified WORK path, to avoid placing results into submodule folder

--- test/test.sh
+++ test/test.sh
@@ -2,5 +2,5 @@
 
 export PATH=/home/ijeras/VLSI/lowrisc-toolchain-gcc-rv32imc-20210412-1/bin/:$PATH
 
-TARGETDIR=`pwd` RISCV_TARGET=r5p make -C ../submodules/riscv-arch-test clean
-TARGETDIR=`pwd` RISCV_TARGET=r5p make -C ../submodules/riscv-arch-test compile
+TARGETDIR=`pwd` RISCV_TARGET=r5p WORK=`pwd`/work make -C ../submodules/riscv-arch-test clean
+TARGETDIR=`pwd` RISCV_TARGET=r5p WORK=`pwd`/work make -C ../submodules/riscv-arch-test compile
