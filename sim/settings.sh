81c82ece4a6978190f47e3a8d89e3f1c4121c544
Iztok Jeras <iztok.jeras@gmail.com>
2021-05-04 09:57:21 +0200

    SIM: removed deprecated code

---
+++ sim/settings.sh
@@ -0,0 +1,5 @@
+export PATH=$HOME/VLSI/verilator/bin/:$PATH
+export VERILATOR_ROOT=$HOME/VLSI/verilator
+#export SYSTEMC_LIBDIR=/opt/systemc-2.3.2/lib-linux64/
+#export SYSTEMC_INCLUDE=/opt/systemc-2.3.2/include/
+
