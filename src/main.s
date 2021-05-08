ad6ab7701f47538a10ed4be3946bf173565cab53
Iztok Jeras <iztok.jeras@gmail.com>
2021-05-08 16:30:48 +0200

    added multiplier code, but it is nt working correctly yet

--- src/main.s
+++ src/main.s
@@ -2,28 +2,25 @@
 
 _start:
 
-ref_1:
-    nop
-    jal x1, halt
-    li t2, 0x01234567
-    nop
-    jal x0, ref_1
-    nop
-    jalr x1, 0xc(t2)
+    li t1, 5
+    li t2, 6
+    mul  t3, t1, t2
+    mulh t4, t1, t2
 
-ref_2:
-    li t2, 0x89abcdef
-    jalr x0, 0(x1)
+    li t1, -5
+    li t2, 6
+    mul  t3, t1, t2
+    mulh t4, t1, t2
 
-ref_3:
-    li x2, 0xa5a5a5a5
-    nop
-    jalr x0, 0(x1)
+    li t1, -0x20000001
+    li t2, -0x20000001
+    mul    t3, t1, t2
+    mulhsu t4, t1, t2
 
 halt:
-    li t1, 0x10000;
-    li t0, 1;
-    sw t0, 0x08(t1);
+    li t1, 0x10000
+    li t0, 1
+    sw t0, 0x08(t1)
 
 finish:
     beq t1, t1, finish
