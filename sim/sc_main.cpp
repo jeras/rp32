b0a963d7759b65ccab43db61a9365ef8853f20c0
Iztok Jeras <iztok.jeras@gmail.com>
2021-12-23 09:04:08 +0100

    SIM: cleaned up top level C++ file, used latest Verilator SystemC example as reference

--- sim/sc_main.cpp
+++ sim/sc_main.cpp
@@ -1,3 +1,6 @@
+// For std::unique_ptr
+#include <memory>
+
 // SystemC global header
 #include <systemc.h>
 
@@ -8,12 +11,7 @@
 // OS tools
 #include <sys/stat.h>  // mkdir
 
-// OVP RISC-V simulator
-//extern "C" {
-//#include "op/op.h"
-//}
-
-// RTL
+// model header generated from SystemVerilog HDL code
 #include "Vr5p_tb.h"
 
 int sc_main(int argc, char **argv) {
@@ -21,21 +19,32 @@ int sc_main(int argc, char **argv) {
     // verilator initialization
     ////////////////////////////////////////////////////////////////////////////
 
+    // Prevent unused variable warnings
+    if (false && argc && argv) {}
+
+    // Create logs/ directory
+    Verilated::mkdir("logs");
+
+    // Before any evaluation, need to know to calculate those signals only used for tracing
+    Verilated::traceEverOn(true);
+
     // parse Verilator arguments
     Verilated::commandArgs(argc, argv);
+
+    // General logfile
+    ios::sync_with_stdio();
 
     // system signals
     sc_clock        clk ("clk", 10, SC_NS, 0.5, 0, SC_NS, true);
     sc_signal<bool> rst;
 
-    Vr5p_tb* top;
-    top = new Vr5p_tb("top");   // SP_CELL (top, Vour);
+    // Construct the Verilated model, from inside Vr5p_tb.h
+    // Using unique_ptr is similar to "Vr5p_tb* top = new Vr5p_tb" then deleting at end
+    const std::unique_ptr<Vr5p_tb> top{new Vr5p_tb{"top"}};
 
+    // Attach Vr5p_tb's signals to this upper model
     top->clk(clk);           // SP_PIN  (top, clk, clk);
     top->rst(rst);           // TODO add a sequence
-
-    // Before any evaluation, need to know to calculate those signals only used for tracing
-    Verilated::traceEverOn(true);
 
     // You must do one evaluation before enabling waves, in order to allow
     // SystemC to interconnect everything for testing.
@@ -43,36 +52,15 @@ int sc_main(int argc, char **argv) {
 
     // If Verilator was invoked with --trace argument,
     // and if at run time passed the +trace argument, turn on tracing
-    VerilatedFstSc* tfp = NULL;
+    VerilatedFstSc* tfp = nullptr;
     const char* flag = Verilated::commandArgsPlusMatch("trace");
     if (flag && 0==strcmp(flag, "+trace")) {
         cout << "Enabling waves into logs/vlt_dump.fst...\n";
         tfp = new VerilatedFstSc;
         top->trace(tfp, 99);
-        mkdir("logs", 0777);
+        Verilated::mkdir("logs");
         tfp->open("logs/vlt_dump.fst");
     }
-
-//    ////////////////////////////////////////////////////////////////////////////
-//    // OVPsim initialization
-//    ////////////////////////////////////////////////////////////////////////////
-//
-//    opSessionInit(OP_VERSION);
-//
-//    opCmdParseStd (argv[0], OP_AC_ALL, argc, (const char**) argv);
-//
-//    optModuleP mr = opRootModuleNew(0, 0, 0);
-//    optModuleP mi = opModuleNew(mr, "OVP_module", "u1", 0, 0);
-//
-//    opRootModulePreSimulate(mr);
-//
-//    // Get processor
-//    optProcessorP processor = opProcessorNext(mi, NULL);
-//
-//    if(!processor)
-//        opMessage("F", "MODULE", "No Processor Found");
-//
-//    opMessage("I", "MODULE", "Trace processor '%s'", opObjectName(processor));
 
     ////////////////////////////////////////////////////////////////////////////
     // test sequence
@@ -92,15 +80,10 @@ int sc_main(int argc, char **argv) {
     // Final model cleanup
     top->final();
     // Close trace if opened
-    if (tfp) { tfp->close(); tfp = NULL; }
-    // cleanup
-    delete top;
-
-//    ////////////////////////////////////////////////////////////////////////////
-//    // OVPsim cleanup
-//    ////////////////////////////////////////////////////////////////////////////
-//
-//    opSessionTerminate();
+    if (tfp) {
+        tfp->close();
+        tfp = nullptr;
+    }
 
     ////////////////////////////////////////////////////////////////////////////
     // exit simulation
