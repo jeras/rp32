bbab51cbcd7445c70d5195ea3d18d496549aba91
Iztok Jeras <iztok.jeras@gmail.com>
2022-02-27 01:35:39 +0100

    FPGA: Altera: cleaning top level RTL ports, PLL is still missing

--- fpga/Altera/DE0-CV/r5p_soc_de0_cv.sv
+++ fpga/Altera/DE0-CV/r5p_soc_de0_cv.sv
@@ -32,18 +32,9 @@
 //   V1.0 :| Yue Yang          :| 08/25/2014:| Initial Revision
 // ============================================================================
 
-//`define Enable_CLOCK2
-//`define Enable_CLOCK3
-//`define Enable_CLOCK4
-`define Enable_CLOCK
 //`define Enable_DRAM
 `define Enable_GPIO
-//`define Enable_HEX0
-//`define Enable_HEX1
-//`define Enable_HEX2
-//`define Enable_HEX3
-//`define Enable_HEX4
-//`define Enable_HEX5
+//`define Enable_HEX
 //`define Enable_KEY
 //`define Enable_LEDR
 //`define Enable_PS2
@@ -53,104 +44,75 @@
 //`define Enable_VGA
 
 module r5p_soc_de0_cv (
-
-`ifdef Enable_CLOCK2
-      ///////// CLOCK2 "3.3-V LVTTL" /////////
-      input              CLOCK2_50,
-`endif
-
-`ifdef Enable_CLOCK3
-      ///////// CLOCK3 "3.3-V LVTTL" /////////
-      input              CLOCK3_50,
-`endif
-
-`ifdef Enable_CLOCK4
-      ///////// CLOCK4  "3.3-V LVTTL"  /////////
-      inout              CLOCK4_50,
-`endif
-`ifdef Enable_CLOCK
-      ///////// CLOCK  "3.3-V LVTTL" /////////
-      input              CLOCK_50,
-`endif
+      // clock inputs
+      input  wire        CLOCK_50,   // CLOCK  "3.3-V LVTTL"
+      input  wire        CLOCK2_50,  // CLOCK2 "3.3-V LVTTL"
+      input  wire        CLOCK3_50,  // CLOCK3 "3.3-V LVTTL"
+      inout  wire        CLOCK4_50,  // CLOCK4 "3.3-V LVTTL"
 `ifdef Enable_DRAM
-      ///////// DRAM  "3.3-V LVTTL" /////////
-      output      [12:0] DRAM_ADDR,
-      output      [1:0]  DRAM_BA,
-      output             DRAM_CAS_N,
-      output             DRAM_CKE,
-      output             DRAM_CLK,
-      output             DRAM_CS_N,
-      inout       [15:0] DRAM_DQ,
-      output             DRAM_LDQM,
-      output             DRAM_RAS_N,
-      output             DRAM_UDQM,
-      output             DRAM_WE_N,
+      // DRAM  "3.3-V LVTTL"
+      output wire [12:0] DRAM_ADDR,
+      output wire  [1:0] DRAM_BA,
+      output wire        DRAM_CAS_N,
+      output wire        DRAM_CKE,
+      output wire        DRAM_CLK,
+      output wire        DRAM_CS_N,
+      inout  wire [15:0] DRAM_DQ,
+      output wire        DRAM_LDQM,
+      output wire        DRAM_RAS_N,
+      output wire        DRAM_UDQM,
+      output wire        DRAM_WE_N,
 `endif
 `ifdef Enable_GPIO
-      ///////// GPIO "3.3-V LVTTL" /////////
-      inout       [35:0] GPIO_0,
-      inout       [35:0] GPIO_1,
+      // GPIO
+      inout  wire [35:0] GPIO_0,  // GPIO "3.3-V LVTTL"
+      inout  wire [35:0] GPIO_1,  // GPIO "3.3-V LVTTL"
 `endif
-`ifdef Enable_HEX0
-      ///////// HEX0  "3.3-V LVTTL" /////////
-      output      [6:0]  HEX0,
-`endif
-`ifdef Enable_HEX1
-      ///////// HEX1 "3.3-V LVTTL" /////////
-      output      [6:0]  HEX1,
-`endif
-`ifdef Enable_HEX2
-      ///////// HEX2 "3.3-V LVTTL" /////////
-      output      [6:0]  HEX2,
-`endif
-`ifdef Enable_HEX3
-      ///////// HEX3 "3.3-V LVTTL" /////////
-      output      [6:0]  HEX3,
-`endif
-`ifdef Enable_HEX4
-      ///////// HEX4 "3.3-V LVTTL" /////////
-      output      [6:0]  HEX4,
-`endif
-`ifdef Enable_HEX5
-      ///////// HEX5 "3.3-V LVTTL" /////////
-      output      [6:0]  HEX5,
+`ifdef Enable_HEX
+      // HEX
+      output wire  [6:0] HEX0,  // HEX0  "3.3-V LVTTL"
+      output wire  [6:0] HEX1,  // HEX1  "3.3-V LVTTL"
+      output wire  [6:0] HEX2,  // HEX2  "3.3-V LVTTL"
+      output wire  [6:0] HEX3,  // HEX3  "3.3-V LVTTL"
+      output wire  [6:0] HEX4,  // HEX4  "3.3-V LVTTL"
+      output wire  [6:0] HEX5,  // HEX5  "3.3-V LVTTL"
 `endif
 `ifdef Enable_KEY
       ///////// KEY  "3.3-V LVTTL" /////////
-      input       [3:0]  KEY,
+      input  wire  [3:0] KEY,
 `endif
 `ifdef Enable_LEDR
       ///////// LEDR /////////
-      output      [9:0]  LEDR,
+      output wire  [9:0] LEDR,
 `endif
 `ifdef Enable_PS2
       ///////// PS2 "3.3-V LVTTL" /////////
-      inout              PS2_CLK,
-      inout              PS2_CLK2,
-      inout              PS2_DAT,
-      inout              PS2_DAT2,
+      inout  wire        PS2_CLK,
+      inout  wire        PS2_CLK2,
+      inout  wire        PS2_DAT,
+      inout  wire        PS2_DAT2,
 `endif
 `ifdef Enable_RESET
       ///////// RESET "3.3-V LVTTL" /////////
-      input              RESET_N
+      input  wire        RESET_N
 `endif
 `ifdef Enable_SD
       ///////// SD "3.3-V LVTTL" /////////
-      output             SD_CLK,
-      inout              SD_CMD,
-      inout       [3:0]  SD_DATA,
+      output wire        SD_CLK,
+      inout  wire        SD_CMD,
+      inout  wire  [3:0] SD_DATA,
 `endif
 `ifdef Enable_SW
       ///////// SW "3.3-V LVTTL"/////////
-      input       [9:0]  SW,
+      input  wire  [9:0] SW,
 `endif
 `ifdef Enable_VGA
       ///////// VGA  "3.3-V LVTTL" /////////
-      output      [3:0]  VGA_B,
-      output      [3:0]  VGA_G,
-      output             VGA_HS,
-      output      [3:0]  VGA_R,
-      output             VGA_VS
+      output wire  [3:0] VGA_B,
+      output wire  [3:0] VGA_G,
+      output wire        VGA_HS,
+      output wire  [3:0] VGA_R,
+      output wire        VGA_VS
 `endif
 );
 
@@ -158,8 +120,7 @@ module r5p_soc_de0_cv (
 // local parameters
 ////////////////////////////////////////////////////////////////////////////////
 
-//localparam int unsigned GW = 32;
-localparam int unsigned GW = 1;
+localparam int unsigned GW = 32;
 
 ///////////////////////////////////////////////////////////////////////////////
 // local signals
