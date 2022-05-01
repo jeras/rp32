42a955cb5bc7d8914d2af73038f58132c72b8058
Iztok Jeras <iztok.jeras@gmail.com>
2022-05-01 14:52:28 +0200

    DEBUG: added TCB decoder, to showcase UHDM issue

--- fpga/Xilinx/yosys/test.tcl
+++ fpga/Xilinx/yosys/test.tcl
@@ -7,7 +7,7 @@ plugin -i systemverilog
 # SystemVerilog RTL
 read_systemverilog \
 +define+ALTERA_RESERVED_QIS \
--top r5p_core \
+-top r5p_soc_top \
 -parse \
 ../../../hdl/rtl/riscv/riscv_isa_pkg.sv \
 ../../../hdl/rtl/core/r5p_bru.sv \
@@ -17,13 +17,13 @@ read_systemverilog \
 ../../../hdl/rtl/core/r5p_wbu.sv \
 ../../../hdl/rtl/core/r5p_core.sv \
 ../../../hdl/rtl/tcb/tcb_if.sv \
-../../../hdl/rtl/soc/r5p_soc_gpio.sv \
-../../../hdl/rtl/tcb/tcb_dec.sv \
 ../../../hdl/rtl/tcb/tcb_arb.sv \
+../../../hdl/rtl/tcb/tcb_dec.sv \
+../../../hdl/rtl/soc/r5p_soc_gpio.sv \
 ../../../hdl/rtl/soc/r5p_soc_mem.sv \
 ../../../hdl/rtl/soc/r5p_soc_top.sv \
 
-synth_xilinx -top r5p_core -edif top.edif
+synth_xilinx -top r5p_core -edif top.edif -flatten -widemux 3 -retime
 
 #hierarchy -top r5p_core
 
