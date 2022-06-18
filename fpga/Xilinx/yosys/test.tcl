8cde005b19f10b173ad1b359d355cb07ab0c7d59
Iztok Jeras <iztok.jeras@gmail.com>
2022-06-18 13:35:43 +0200

    TCB: moving code for GPIO/UART peripherals into dedicated folders

--- fpga/Xilinx/yosys/test.tcl
+++ fpga/Xilinx/yosys/test.tcl
@@ -20,8 +20,11 @@ read_systemverilog \
 ../../../submodules/tcb/hdl/rtl/tcb_if.sv \
 ../../../submodules/tcb/hdl/rtl/tcb_dec.sv \
 ../../../submodules/tcb/hdl/rtl/tcb_arb.sv \
-../../../submodules/tcb/hdl/rtl/tcb_gpio.sv \
-../../../submodules/tcb/hdl/rtl/tcb_uart.sv \
+../../../submodules/tcb/hdl/rtl/gpio/tcb_gpio.sv \
+../../../submodules/tcb/hdl/rtl/uart/tcb_uart_ser.sv \
+../../../submodules/tcb/hdl/rtl/uart/tcb_uart_des.sv \
+../../../submodules/tcb/hdl/rtl/uart/tcb_uart_fifo.sv \
+../../../submodules/tcb/hdl/rtl/uart/tcb_uart.sv \
 ../../../hdl/rtl/soc/r5p_soc_mem.sv \
 ../../../hdl/rtl/soc/r5p_soc_top.sv \
 
