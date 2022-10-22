e371fd63297f685a7c0a66fa7182bd978cbf644e
Iztok Jeras <iztok.jeras@cosylab.com>
2022-10-22 15:28:48 +0200

    SIM: removing remnants of SystemC, C++ code

--- sim/verilator/settings.sh
+++ sim/verilator/settings.sh
@@ -1,7 +1,4 @@
 GIT_TOPLEVEL=`git rev-parse --show-toplevel`
 export PATH=$GIT_TOPLEVEL/submodules/verilator/bin/:$PATH
 export VERILATOR_ROOT=$GIT_TOPLEVEL/submodules/verilator
-#export LD_LIBRARY_PATH=/opt/systemc-2.3.3/lib-linux64/
-#export SYSTEMC_LIBDIR=/opt/systemc-2.3.3/lib-linux64/
-#export SYSTEMC_INCLUDE=/opt/systemc-2.3.3/include/
 
