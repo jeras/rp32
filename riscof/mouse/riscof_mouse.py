baa60f5b8ec2228769e1af416ea71820d0b7f82a
Iztok Jeras <iztok.jeras@gmail.com>
2025-03-27 00:38:24 +0100

    RISCOF cleanup

--- riscof/mouse/riscof_mouse.py
+++ riscof/mouse/riscof_mouse.py
@@ -1,13 +1,5 @@
 import os
-import re
-import shutil
-import subprocess
-import shlex
 import logging
-import random
-import string
-from string import Template
-import sys
 
 import riscof.utils as utils
 import riscof.constants as constants
@@ -18,7 +10,7 @@ logger = logging.getLogger()
 class mouse(pluginTemplate):
     __model__ = "mouse"
 
-    #TODO: please update the below to indicate family, version, etc of your DUT.
+    # TODO: please update the below to indicate family, version, etc of your DUT.
     __version__ = "XXX"
 
     def __init__(self, *args, **kwargs):
@@ -26,22 +18,10 @@ class mouse(pluginTemplate):
 
         config = kwargs.get('config')
 
-        # If the config node for this DUT is missing or empty. Raise an error. At minimum we need
-        # the paths to the ispec and pspec files
+        # If the config node for this DUT is missing or empty. Raise an error.
+        # At minimum we need the paths to the ispec and pspec files.
         if config is None:
             print("Please enter input file paths in configuration.")
-            raise SystemExit(1)
-
-        # In case of an RTL based DUT, this would be point to the final binary executable of your
-        # test-bench produced by a simulator (like verilator, vcs, incisive, etc). In case of an iss or
-        # emulator, this variable could point to where the iss binary is located. If 'PATH variable
-        # is missing in the config.ini we can hardcode the alternate here.
-        if   (config['simulator'] == "questa"):
-            self.dut_exe = "make -C ../../sim/questa/ -f Makefile.mouse"
-        elif (config['simulator'] == "verilator"):
-            self.dut_exe = "make -C ../../sim/verilator/ -f Makefile.mouse"
-        else:
-            print("No simulator selected for '{__model__}'.")
             raise SystemExit(1)
 
         # Number of parallel jobs that can be spawned off by RISCOF
@@ -49,166 +29,180 @@ class mouse(pluginTemplate):
         # parallel on the DUT executable. Can also be used in the build function if required.
         self.num_jobs = str(config['jobs'] if 'jobs' in config else 1)
 
-        # Path to the directory where this python file is located. Collect it from the config.ini
-        self.pluginpath=os.path.abspath(config['pluginpath'])
+        # Path to the directory where this python file is located. Collect it from the config.ini.
+        self.pluginpath = os.path.abspath(config['pluginpath'])
 
-        # Collect the paths to the  riscv-config absed ISA and platform yaml files. One can choose
-        # to hardcode these here itself instead of picking it from the config.ini file.
+        # Collect the paths to the  riscv-config absed ISA and platform yaml files.
+        # One can choose to hardcode these here itself instead of picking it from the config.ini file.
         self.isa_spec = os.path.abspath(config['ispec'])
         self.platform_spec = os.path.abspath(config['pspec'])
 
-        #We capture if the user would like the run the tests on the target or
-        #not. If you are interested in just compiling the tests and not running
-        #them on the target, then following variable should be set to False
+        # We capture if the user would like the run the tests on the target or not.
+        # If you are interested in just compiling the tests and not running them on the target,
+        # then following variable should be set to False.
         if 'target_run' in config and config['target_run']=='0':
             self.target_run = False
         else:
             self.target_run = True
 
+        # Capture HDL simulator choice.
+        self.simulator = config['simulator']
+
     def initialise(self, suite, work_dir, archtest_env):
 
-       # capture the working directory. Any artifacts that the DUT creates should be placed in this
-       # directory. Other artifacts from the framework and the Reference plugin will also be placed
-       # here itself.
-       self.work_dir = work_dir
+        # Capture the working directory.
+        # Any artifacts that the DUT creates should be placed in this directory.
+        # Other artifacts from the framework and the Reference plugin will also be placed here.
+        self.work_dir = work_dir
 
-       # capture the architectural test-suite directory.
-       self.suite_dir = suite
+        # Capture the architectural test-suite directory.
+        self.suite_dir = suite
 
-       # Note the march is not hardwired here, because it will change for each
-       # test. Similarly the output elf name and compile macros will be assigned later in the
-       # runTests function
-       self.compile_cmd = 'riscv{0}-unknown-elf-gcc -march={1} \
-         -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -g\
-         -T '+self.pluginpath+'/env/link.ld\
-         -I '+self.pluginpath+'/env/\
-         -I ' + archtest_env + ' {2} -o {3} {4}'
+        # Capture the environment.
+        self.archtest_env = archtest_env
 
-       # add more utility snippets here
+        # In case of an RTL based DUT, this would be point to the final binary executable of your
+        # test-bench produced by a simulator (like verilator, vcs, incisive, etc).
+        # In case of an iss or emulator, this variable could point to where the iss binary is located.
+        # If PATH variable is missing in the config.ini we can hardcode the alternate here.
+        # TODO: PATH?
+        if   (self.simulator == 'questa'):
+            self.dut_exe = f'make -C {os.path.join(self.work_dir, "../../sim/questa/")} -f Makefile.mouse'
+        elif (self.simulator == 'verilator'):
+            self.dut_exe = f'make -C {os.path.join(self.work_dir, "../../sim/verilator/")} -f Makefile.mouse'
+        else:
+            # TODO: __model__ ?
+            print("No simulator selected for '{__model__}'.")
+            raise SystemExit(1)
 
     def build(self, isa_yaml, platform_yaml):
 
         # load the isa yaml as a dictionary in python.
         ispec = utils.load_yaml(isa_yaml)['hart0']
 
-        # capture the XLEN value by picking the max value in 'supported_xlen' field of isa yaml. This
-        # will be useful in setting integer value in the compiler string (if not already hardcoded);
+        # Capture the XLEN value by picking the max value in 'supported_xlen' field of isa YAML.
+        # This will be useful in setting integer value in the compiler string (if not already hardcoded);
         self.xlen = ('64' if 64 in ispec['supported_xlen'] else '32')
 
-        # for mouse start building the '--isa' argument. the self.isa is dutname specific and may not be
-        # useful for all DUTs
+        # For DUT start building the '--isa' argument.
+        # The self.isa is DUT specific and may not be useful for all DUTs.
         self.isa = 'rv' + self.xlen
         for ext in ['I', 'M', 'C', 'F', 'D']:
             if ext in ispec["ISA"]:
                 self.isa += ext.lower()
 
-        #TODO: The following assumes you are using the riscv-gcc toolchain. If
-        #      not please change appropriately
-        self.compile_cmd = self.compile_cmd+' -mabi='+('lp64 ' if 64 in ispec['supported_xlen'] else 'ilp32 ')
+        # Note the march is not hardwired here, because it will change for each test.
+        # Similarly the output elf name and compile macros will be assigned later in the runTests function.
+        self.compile_exe = f'riscv{self.xlen}-unknown-elf-gcc'
+        self.objcopy_exe = f'riscv{self.xlen}-unknown-elf-objcopy'
+        self.objdump_exe = f'riscv{self.xlen}-unknown-elf-objdump'
 
     def runTests(self, testList):
 
-        # Delete Makefile if it already exists.
-        if os.path.exists(self.work_dir+ "/Makefile." + self.name[:-1]):
-                os.remove(self.work_dir+ "/Makefile." + self.name[:-1])
-        # create an instance the makeUtil class that we will use to create targets.
-        make = utils.makeUtil(makefilePath=os.path.join(self.work_dir, "Makefile." + self.name[:-1]))
+        # TUDO: figure out why there is an extra character in the name.
+        name = self.name[:-1]
 
-        # set the make command that will be used. The num_jobs parameter was set in the __init__
-        # function earlier
+        # Delete Makefile if it already exists.
+        if os.path.exists(self.work_dir+ "/Makefile." + name):
+                os.remove(self.work_dir+ "/Makefile." + name)
+        # create an instance the makeUtil class that we will use to create targets.
+        make = utils.makeUtil(makefilePath=os.path.join(self.work_dir, "Makefile." + name))
+
+        # Set the make command that will be used.
+        # The num_jobs parameter was set in the __init__ function earlier
         make.makeCommand = 'make -k -j' + self.num_jobs
 
-        # we will iterate over each entry in the testList. Each entry node will be refered to by the
-        # variable testname.
+        # We will iterate over each entry in the testList.
+        # Each entry node will be referred to by the variable testname.
         for testname in testList:
 
-            # for each testname we get all its fields (as described by the testList format)
+            # For each testname we get all its fields (as described by the testList format).
             testentry = testList[testname]
 
-            # we capture the path to the assembly file of this test
+            # We capture the path to the assembly file of this test.
             test = testentry['test_path']
 
-            # capture the directory where the artifacts of this test will be dumped/created. RISCOF is
-            # going to look into this directory for the signature files
+            # Capture the directory where the artifacts of this test will be dumped/created.
+            # RISCOF is going to look into this directory for the signature files.
             test_dir = testentry['work_dir']
 
-            # name of the elf file after compilation of the test
+            # Name of the elf file after compilation of the test.
             elf = 'dut.elf'
 
-            # name of the signature file as per requirement of RISCOF. RISCOF expects the signature to
-            # be named as DUT-<dut-name>.signature. The below variable creates an absolute path of
-            # signature file.
-            sig_file = os.path.join(test_dir, self.name[:-1] + ".signature")
+            # Name of the signature file as per requirement of RISCOF.
+            # RISCOF expects the signature to be named as DUT-<dut-name>.signature.
+            # The below variable creates an absolute path of signature file.
+            sig_file = os.path.join(test_dir, name + ".signature")
 
-            # for each test there are specific compile macros that need to be enabled. The macros in
-            # the testList node only contain the macros/values. For the gcc toolchain we need to
-            # prefix with "-D". The following does precisely that.
-            compile_macros= ' -D' + " -D".join(testentry['macros'])
+            # For each test there are specific compile macros that need to be enabled.
+            # The macros in the testList node only contain the macros/values.
+            # For the gcc toolchain we need to prefix with "-D". The following does precisely that.
+            compile_macros = ' '.join([f'-D{macro}' for macro in testentry['macros']])
 
-            # substitute all variables in the compile command that we created in the initialize
-            # function
-            elfcmd = self.compile_cmd.format(self.xlen, testentry['isa'].lower(), test, elf, compile_macros)
+            # Construct the command line for compiling testcase source assembly into an elf file.
+            compile_cmd = self.compile_exe + (
+                f' -mabi={'lp64' if (self.xlen == 64) else 'ilp32'}'
+                f' -march={testentry['isa'].lower()}'
+                f' -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -g'
+                f' -T {self.pluginpath}/env/link.ld'
+                f' -I {self.pluginpath}/env/'
+                f' -I {self.archtest_env}'
+                f' {compile_macros}'
+                f' {test} -o {elf}'
+            )
 
-            # command for converting elf file into a binary for loading RTL simulation memory
-            bincmd = f'riscv{self.xlen}-unknown-elf-objcopy -O binary {elf} {elf}.bin'
+            # Command for converting elf file into a binary/hex file for loading into HDL testbench memory.
+            # Uncomment either the binary or hex version, depending on your.
+            objcopy_cmd = self.objcopy_exe + f' -O binary {elf} {elf}.bin'
+            #objcopy_cmd = self.objcopy_exe + f' -O binary {elf} {elf}.hex'
 
-            # disassembler
-            disasscmd = f'riscv{self.xlen}-unknown-elf-objdump -M no-aliases -M numeric -D {elf} > {elf}.disass'
+            # Disassemble the ELF file for debugging purposes
+            objdump_cmd = self.objdump_exe + (
+                f' -M no-aliases -M numeric'
+                f' -D {elf} > {elf}.disass'
+            )
 
-#            # get symbols from elf file
-#            symbols_dump = f'riscv{self.xlen}-unknown-elf-objdump -t {elf}'
-#            # extract listed symbols
-#            symbols_list = ['begin_signature', 'end_signature', 'tohost', 'fromhost']
-#            # construct dictionary of listed symbols
-#            symbols_dict = {}
-#            for line in symbols_dump.splitlines():
-#                for symbol in symbols_list:
-#                    if symbol in line:
-#                        print(line)
-#                        symbols_dict.update({symbol: line[0:7]})
-
-            # construct dictionary of file paths
-            files_dict = {
+            # Construct Verilog plusargs dictionary containing file paths.
+            plusargs_dict = {
                 'firmware': os.path.join(test_dir, elf)+'.bin',
                 'signature': sig_file
             }
 
-            # construct plusargs string from symbols and file paths
-#            plusargs_dict = symbols_dict | files_dict
-            plusargs_dict = files_dict
-            plusargs = ' '.join([f'+{key}={val}' for key, val in plusargs_dict.items()])
+            # Other DUT testbench specific Verilog plusargs can be added here.
+            plusargs_dict.update({})
 
-	        # if the user wants to disable running the tests and only compile the tests, then
-	        # the "else" clause is executed below assigning the sim command to simple no action
-	        # echo statement.
+	        # If the user wants to disable running the tests and only compile the tests,
+            # then the "else" clause is executed below assigning the sim command to simple no action echo statement.
             if self.target_run:
                 # set up the simulation command. Template is for spike. Please change.
-                simcmd = self.dut_exe + f' ARG="{plusargs}"'
+                sim_cmd = self.dut_exe + (
+                    f' RISCOF_ARGUMENTS="{' '.join([f'+{key}={val}' for key, val in plusargs_dict.items()])}"'
+                )
             else:
-                simcmd = 'echo "NO RUN"'
+                sim_cmd = 'echo "NO RUN"'
 
-            # concatenate all commands that need to be executed within a make-target.
+            # Concatenate all commands that need to be executed within a make-target.
             execute = []
             execute.append(f'cd {test_dir}')
-            execute.append(f'{elfcmd}')
-            execute.append(f'{bincmd}')
-            execute.append(f'{disasscmd}')
-            execute.append(f'cd -')
-            execute.append(f'{simcmd}')
+            execute.append(compile_cmd)
+            execute.append(objcopy_cmd)
+            execute.append(objdump_cmd)
+            execute.append('cd -')
+            execute.append(sim_cmd)
 
-            # create a target. The makeutil will create a target with the name "TARGET<num>" where num
-            # starts from 0 and increments automatically for each new target that is added
+            # Create a target.
+            # The makeutil will create a target with the name "TARGET<num>" where
+            # num starts from 0 and increments automatically for each new target that is added.
             make.add_target('@' + ';\\\n'.join(execute))
 
-        # if you would like to exit the framework once the makefile generation is complete uncomment the
-        # following line. Note this will prevent any signature checking or report generation.
+        # If you would like to exit the framework once the makefile generation is complete uncomment the following line.
+        # Note this will prevent any signature checking or report generation.
         #raise SystemExit
 
-        # once the make-targets are done and the makefile has been created, run all the targets in
-        # parallel using the make command set above.
+        # Once the make-targets are done and the makefile has been created,
+        # run all the targets in parallel using the make command set above.
         make.execute_all(self.work_dir)
 
-        # if target runs are not required then we simply exit as this point after running all
-        # the makefile targets.
+        # If target runs are not required then we simply exit as this point after running all the makefile targets.
         if not self.target_run:
             raise SystemExit(0)
