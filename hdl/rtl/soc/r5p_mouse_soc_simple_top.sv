37c350c85e24654e994adb44fb2530fd01862cde
Iztok Jeras <iztok.jeras@gmail.com>
2026-01-31 13:41:47 +0100

    replaced 2** operator with 1<< operator

--- hdl/rtl/soc/r5p_mouse_soc_simple_top.sv
+++ hdl/rtl/soc/r5p_mouse_soc_simple_top.sv
@@ -57,8 +57,6 @@ module r5p_mouse_soc_simple_top
 // local parameters and parameter validation
 ////////////////////////////////////////////////////////////////////////////////
 
-    localparam MEM_ADR = $clog2(MEM_SIZ);
-
 // TODO: check if instruction address bus width and instruction memory size fit
 // TODO: check if data address bus width and data memory size fit
 
@@ -103,8 +101,6 @@ module r5p_mouse_soc_simple_top
         .tcb_rdy (tcb_cpu.rdy)
     );
 
-    assign tcb_cpu.req.byt = '1;
-
     // signals not provided by the CPU
     assign tcb_cpu.req.lck = 1'b0;
     assign tcb_cpu.req.ndn = 1'b0;
@@ -143,6 +139,36 @@ module r5p_mouse_soc_simple_top
         .man  (tcb_mem)
     );
 
+    // register request path to convert from DLY=1 CPU to DLY=0 peripherals
+    tcb_lite_lib_register_request tcb_lsu_register (
+        .sub  (tcb_dmx[1]),
+        .man  (tcb_pb0)
+    );
+
+    logic [$clog2(2)-1:0] tcb_pb0_sel;
+
+    // decoding peripherals (GPIO/UART)
+    tcb_lite_lib_decoder #(
+        .ADR (CFG_CPU.BUS.ADR),
+        .IFN (2),
+        .DAM ({{17'bx, 15'bxx_xxxx_x1xx_xxxx},   // 0x40 ~ 0x7f - UART controller
+               {17'bx, 15'bxx_xxxx_x0xx_xxxx}})  // 0x00 ~ 0x3f - GPIO controller
+    ) tcb_pb0_dec (
+        .mon  (tcb_pb0),
+        .sel  (tcb_pb0_sel)
+    );
+
+    // demultiplexing peripherals (GPIO/UART)
+    tcb_lite_lib_demultiplexer #(
+        .IFN (2)
+    ) tcb_pb0_demux (
+        // control
+        .sel  (tcb_pb0_sel),
+        // TCB interfaces
+        .sub  (tcb_pb0),
+        .man  (tcb_per)
+    );
+
 ////////////////////////////////////////////////////////////////////////////////
 // memory instances
 ////////////////////////////////////////////////////////////////////////////////
@@ -159,67 +185,89 @@ module r5p_mouse_soc_simple_top
 // GPIO
 ////////////////////////////////////////////////////////////////////////////////
 
-    logic [GPIO_DAT-1:0] gpio_r [1:0];
+    generate
+    if (ENA_GPIO) begin: gen_gpio
 
-    // input resynchronization
-    always @(posedge clk)
-    begin
-        gpio_r[0] <= gpio_i;
-        gpio_r[1] <= gpio_r[0];
-    end
+        // GPIO controller
+        tcb_lite_peri_gpio #(
+            .GPIO_DAT (GPIO_DAT),
+            .SYS_MIN  (1'b1)
+        ) gpio (
+            // GPIO signals
+            .gpio_o  (gpio_o),
+            .gpio_e  (gpio_e),
+            .gpio_i  (gpio_i),
+            // bus interface
+            .sub     (tcb_per[0]),
+            .irq     ()
+        );
 
-    logic               per_ena;  // enable
-    logic               per_wen;  // write enable
-    logic [MEM_ADR-1:2] per_adr;  // address
-    logic    [XLEN-1:0] per_wdt;  // write data
+    end: gen_gpio
+    else begin: gen_gpio_err
 
-    // delayed peripheral transfer
-    always_ff @(posedge clk, posedge rst)
-    if (rst)  per_ena <= 1'b0;
-    else      per_ena <= tcb_dmx[1].trn;
+        // error response
+        tcb_lite_lib_error gpio_err (
+            .sub  (tcb_per[0]),
+            .sts  ('0)
+        );
 
-    // delayed peripheral request
-    always_ff @(posedge clk)
-    if (tcb_dmx[1].trn) begin
-        per_wen <= tcb_cpu.req.wen;
-        per_adr <= tcb_cpu.req.adr[MEM_ADR-1:2];
-        per_wdt <= tcb_cpu.req.wdt;
-    end
+        // GPIO signals
+        assign gpio_o = '0;
+        assign gpio_e = '0;
+        //     gpio_i
 
-    // GPIO write access
-    always_ff @(posedge clk, posedge rst)
-    if (rst) begin
-        gpio_o <= '0;
-        gpio_e <= '0;
-    end else begin
-        if (per_ena & per_wen) begin
-            if (per_adr[2] == 1'b0) gpio_o <= per_wdt[GPIO_DAT-1:0];
-            if (per_adr[2] == 1'b1) gpio_e <= per_wdt[GPIO_DAT-1:0];
-        end
-    end
-
-    always_comb
-    case (per_adr[3:2])
-        2'b00:    tcb_dmx[1].rsp.rdt = gpio_o;
-        2'b01:    tcb_dmx[1].rsp.rdt = gpio_e;
-        2'b10:    tcb_dmx[1].rsp.rdt = gpio_r[1];
-        default:  tcb_dmx[1].rsp.rdt = 'x;
-    endcase
-
-    // there are no error conditions
-    assign tcb_dmx[1].rsp.sts =   '0;
-    assign tcb_dmx[1].rsp.err = 1'b0;
-
-    // there is no backpressure
-    assign tcb_dmx[1].rdy = 1'b1;
+    end: gen_gpio_err
+    endgenerate
 
 ////////////////////////////////////////////////////////////////////////////////
 // UART
 ////////////////////////////////////////////////////////////////////////////////
 
-    // UART is just a loopback
-    // register used to avoid combinational loop in testbench
-    always_ff @(posedge clk)
-    uart_txd <= uart_rxd;
+    generate
+    if (ENA_UART) begin: gen_uart
+
+        // baudrate parameters (divider and counter width)
+        localparam int unsigned BDR = 50_000_000 / 115_200;  // 50MHz / 115200 = 434.0
+        localparam int unsigned UART_BDR = $clog2(BDR);  // a 9-bit counter is required
+
+        // UART controller
+        tcb_lite_peri_uart #(
+            // UART parameters
+            .UART_BDR       (UART_BDR),
+            .FIFO_SIZ       (FIFO_SIZ),
+            // configuration register parameters (write enable, reset value)
+            .CFG_TX_BDR_WEN (1'b0),  .CFG_TX_BDR_RST (UART_BDR'(BDR)),
+            .CFG_TX_IRQ_WEN (1'b0),  .CFG_TX_IRQ_RST ('x),
+            .CFG_RX_BDR_WEN (1'b0),  .CFG_RX_BDR_RST (UART_BDR'(BDR)),
+            .CFG_RX_SMP_WEN (1'b0),  .CFG_RX_SMP_RST (UART_BDR'(BDR/2)),
+            .CFG_RX_IRQ_WEN (1'b0),  .CFG_RX_IRQ_RST ('x),
+            // TCB parameters
+            .SYS_MIN (1'b1)
+        ) uart (
+            // UART signals
+            .uart_txd (uart_txd),
+            .uart_rxd (uart_rxd),
+            // TCB-Lite interface
+            .sub      (tcb_per[1]),
+            // interrupts
+            .irq_tx   (),
+            .irq_rx   ()
+        );
+
+    end: gen_uart
+    else begin: gen_uart_err
+
+        // error response
+        tcb_lite_lib_error uart_err (
+            .sub (tcb_per[1]),
+            .sts  ('x)
+        );
+
+        // UART signals
+        assign uart_txd = 1'b1;
+        //     uart_rxd
+
+    end: gen_uart_err
+    endgenerate
 
 endmodule: r5p_mouse_soc_simple_top
